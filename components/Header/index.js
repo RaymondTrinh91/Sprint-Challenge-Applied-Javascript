4a73ebe17e4035a239ca899887229534caf77119
Raymond Trinh <RaymondTrinh91@gmail.com>
2019-10-04 08:18:03 -0700

    Sprint Challenge Start

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,31 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    //Elements
+    const
+    head = document.createElement("div"),
+    date = document.createElement("span"),
+    title = document.createElement("h1"),
+    temp = document.createElement("span");
+
+    //Structure
+    head.appendChild(date);
+    head.appendChild(title);
+    head.appendChild(temp);
+
+    //Classes
+    head.classList.add("header");
+    date.classList.add("date");
+    temp.classList.add("temp");
+
+    //Content
+    date.textContent = "SMARCH 28, 2019";
+    title.textContent = "Lambda Times";
+    temp.textContent = "98°";
+
+    return head;
+}
+
+const headContainer = document.querySelector(".header-container");
+headContainer.appendChild(Header());
